3fe8058361dd738f3fbccca7b8ee1d731a445190
Guilherme de Lima <guilhermel_ima@hotmail.com>
2021-02-22 20:53:09 -0300

    refatoração

---
+++ src/test/java/MassaDados.java
@@ -0,0 +1,11 @@
+public class MassaDados {
+    //id do voto
+    String vote_id;
+    //cadastro
+    String urlCadastro= "user/passwordlesssignup";
+    String bodyCadastro="{\"email\": \"guilhermel_ima@hotmail.com\",\"appDescription\": \"teste api\"}";
+    //votacao
+    String corpoVotacao="{\"image_id\": \"rzPxlNge1\", \"value\": \"true\", \"sub_id\": \"demo-cbd3aa\"}";
+    //favoritar
+    String corpoFavorita="{\"image_id\": \"9ccXTANkb\"}";
+}
--- src/test/java/TesteApi.java
+++ src/test/java/TesteApi.java
@@ -1,55 +1,50 @@
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
-public class TesteApi {
+public class TesteApi extends MassaDados{
 
-    String vote_id;
-
+    @BeforeClass
+    public static void urlbase(){
+        baseURI = "https://api.thecatapi.com/v1/";
+    }
     @Test
     public void  cadastro() {
-        String url= "https://api.thecatapi.com/v1/user/passwordlesssignup";
-        String bodymessage="{\"email\": \"guilhermel_ima@hotmail.com\",\"appDescription\": \"teste api\"}";
-
-        Response response = given().contentType("application/json").body(bodymessage).
-                when().post(url);
-
-                response.then().body("message",containsString("SUCCESS")).statusCode(200);
-
-                System.out.println("Retorno =>" + response.body().asString());
+        Response response = given().contentType("application/json").body(bodyCadastro).
+                when().post(urlCadastro);
+                validacao(response);
     }
 
     @Test
-    public void  votos() {
-        String url= "https://api.thecatapi.com/v1/votes/";
-
-        Response response = given().contentType("application/json").body("{\"image_id\": \"rzPxlNge1\", \"value\": \"true\", \"sub_id\": \"demo-cbd3aa\"}").when().post(url);
+    public void  votacao() {
+        Response response = given().contentType("application/json")
+                .body(corpoVotacao)
+                .when().post("votes/");
 
         response.then().body("message",containsString("SUCCESS")).statusCode(200);
+        validacao(response);
 
-        System.out.println("Retorno =>" + response.body().asString());
         String id = response.jsonPath().getString("id");
         vote_id = id;
         System.out.println("ID =>" + id);
     }
     
-    @Test
+    //@Test
     public void  deletavotacao() {
         //erro
-        votos();
+        votacao();
         deletaVoto();
     }
 
     private void deletaVoto() {
-        String url= "https://api.thecatapi.com/v1/votes/{vote_id}";
-        Response response = given().contentType("application/json").header("x-api-key","e7f3c42a-7986-4bd8-a955-efaf081e334a").pathParam("vote_id", vote_id).when().delete(url);
+        String url= "votes/{vote_id}";
+        Response response = given().contentType("application/json").header("x-api-key","a7cf904d-4182-4921-8e35-7e82fb1cc354").pathParam("vote_id", vote_id).when().delete(url);
 
-        System.out.println("Retorno =>" + response.body().asString());
-
-        response.then().body("message",containsString("SUCCESS")).statusCode(200);
-
+        validacao(response);
     }
 
     @Test
@@ -60,30 +55,30 @@ public class TesteApi {
     }
 
     private void favorita() {
-        String url= "https://api.thecatapi.com/v1/favourites";
         Response response =
                 given()
                 .contentType("application/json")
-                .header("x-api-key","e7f3c42a-7986-4bd8-a955-efaf081e334a")
-                .body("{\"image_id\": \"9ccXTANkb\"}").when().post(url);
+                .header("x-api-key","a7cf904d-4182-4921-8e35-7e82fb1cc354")
+                .body(corpoFavorita).when().post("favourites");
+
                 String id = response.jsonPath().getString("id");
                 vote_id = id;
 
-        System.out.println("Retorno Favorita =>" + response.body().asString());
-        response.then().body("message",containsString("SUCCESS")).statusCode(200);
-
+                validacao(response);
     }
     private void desfavorita() {
-        String url= "https://api.thecatapi.com/v1/favourites/{favourite_id}";
+        String url= "favourites/{favourite_id}";
         Response response =
                 given().contentType("application/json")
-                        .header("x-api-key","e7f3c42a-7986-4bd8-a955-efaf081e334a")
+                        .header("x-api-key","a7cf904d-4182-4921-8e35-7e82fb1cc354")
                         .pathParam("favourite_id", vote_id)
                         .when().delete(url);
 
-        System.out.println("Retorno Desfavorita =>" + response.body().asString());
+                        validacao(response);
+    }
+
+    public void validacao(Response response){
         response.then().body("message",containsString("SUCCESS")).statusCode(200);
-
-
+        System.out.println("Retorno Da Api =>" + response.body().asString());
     }
 }
