e225849d017727ab2cc3b9dad8c5e3f215ad6c67
Guilherme de Lima <guilhermel_ima@hotmail.com>
2021-02-22 20:25:06 -0300

    test favoritar

--- src/test/java/TesteApi.java
+++ src/test/java/TesteApi.java
@@ -5,6 +5,9 @@ import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
 public class TesteApi {
+
+    String vote_id;
+
     @Test
     public void  cadastro() {
         String url= "https://api.thecatapi.com/v1/user/passwordlesssignup";
@@ -28,6 +31,59 @@ public class TesteApi {
 
         System.out.println("Retorno =>" + response.body().asString());
         String id = response.jsonPath().getString("id");
-            System.out.println("ID =>" + id);
+        vote_id = id;
+        System.out.println("ID =>" + id);
+    }
+    
+    @Test
+    public void  deletavotacao() {
+        //erro
+        votos();
+        deletaVoto();
+    }
+
+    private void deletaVoto() {
+        String url= "https://api.thecatapi.com/v1/votes/{vote_id}";
+        Response response = given().contentType("application/json").header("x-api-key","e7f3c42a-7986-4bd8-a955-efaf081e334a").pathParam("vote_id", vote_id).when().delete(url);
+
+        System.out.println("Retorno =>" + response.body().asString());
+
+        response.then().body("message",containsString("SUCCESS")).statusCode(200);
+
+    }
+
+    @Test
+    public void  favoritar() {
+
+        favorita();
+        desfavorita();
+    }
+
+    private void favorita() {
+        String url= "https://api.thecatapi.com/v1/favourites";
+        Response response =
+                given()
+                .contentType("application/json")
+                .header("x-api-key","e7f3c42a-7986-4bd8-a955-efaf081e334a")
+                .body("{\"image_id\": \"9ccXTANkb\"}").when().post(url);
+                String id = response.jsonPath().getString("id");
+                vote_id = id;
+
+        System.out.println("Retorno Favorita =>" + response.body().asString());
+        response.then().body("message",containsString("SUCCESS")).statusCode(200);
+
+    }
+    private void desfavorita() {
+        String url= "https://api.thecatapi.com/v1/favourites/{favourite_id}";
+        Response response =
+                given().contentType("application/json")
+                        .header("x-api-key","e7f3c42a-7986-4bd8-a955-efaf081e334a")
+                        .pathParam("favourite_id", vote_id)
+                        .when().delete(url);
+
+        System.out.println("Retorno Desfavorita =>" + response.body().asString());
+        response.then().body("message",containsString("SUCCESS")).statusCode(200);
+
+
     }
 }
